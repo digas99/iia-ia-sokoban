34d6b5e6c5c9728e21bebf212a440c78f100960d
Diogo Gomes <dgomes@ua.pt>
2020-11-11 16:43:58 +0000

    new look

--- prof/grading.py
+++ prof/grading.py
@@ -1,14 +1,19 @@
+import os
+import sys
+
 from flask import Flask, request, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
 from sqlalchemy import and_, func
 
-from ..game import reduce_score
+sys.path.append(os.getcwd() + "/..")
+from game import reduce_score
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path="")
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'grades.sqlite')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "grades.sqlite"
+)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -25,7 +30,17 @@ class Game(db.Model):
     box_on_goal = db.Column(db.Integer)
     score = db.Column(db.Integer)
 
-    def __init__(self, player, level, puzzles, total_steps, total_moves, total_pushes, box_on_goal, score):
+    def __init__(
+        self,
+        player,
+        level,
+        puzzles,
+        total_steps,
+        total_moves,
+        total_pushes,
+        box_on_goal,
+        score,
+    ):
         self.player = player
         self.level = level
         self.puzzles = puzzles
@@ -35,10 +50,22 @@ class Game(db.Model):
         self.box_on_goal = box_on_goal
         self.score = score
 
+
 class GameSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('id', 'timestamp', 'player', 'level', 'puzzles', 'total_steps', 'total_moves', 'total_pushes', 'box_on_goal', 'score')
+        fields = (
+            "id",
+            "timestamp",
+            "player",
+            "level",
+            "puzzles",
+            "total_steps",
+            "total_moves",
+            "total_pushes",
+            "box_on_goal",
+            "score",
+        )
 
 
 game_schema = GameSchema()
@@ -48,19 +75,28 @@ games_schema = GameSchema(many=True)
 # endpoint to create new game
 @app.route("/game", methods=["POST"])
 def add_game():
-    player = request.json['player']
-    level = request.json['level']
-    puzzles = request.json.get('puzzles', level)
-    total_steps = request.json.get('total_steps', -1)
-    total_moves = request.json.get('total_moves', -1)
-    total_pushes = request.json.get('total_pushes', -1)
-    box_on_goal = request.json.get('box_on_goal', 0)
-    papertrail = request.json.get('papertrail', "")
+    player = request.json["player"]
+    level = request.json["level"]
+    puzzles = request.json.get("puzzles", level)
+    total_steps = request.json.get("total_steps", -1)
+    total_moves = request.json.get("total_moves", -1)
+    total_pushes = request.json.get("total_pushes", -1)
+    box_on_goal = request.json.get("box_on_goal", 0)
+    papertrail = request.json.get("papertrail", "")
 
     score = reduce_score(puzzles, total_moves, total_pushes, total_steps, box_on_goal)
 
     print(player, level, puzzles, score, papertrail)
-    new_game = Game(player, level, puzzles, total_steps, total_moves, total_pushes, box_on_goal, score)
+    new_game = Game(
+        player,
+        level,
+        puzzles,
+        total_steps,
+        total_moves,
+        total_pushes,
+        box_on_goal,
+        score,
+    )
 
     if puzzles == level:
         db.session.add(new_game)
@@ -68,17 +104,32 @@ def add_game():
 
     return game_schema.jsonify(new_game)
 
+
 @app.route("/static/<path:path>")
 def send_static(path):
-    return send_from_directory('static', path)
+    return send_from_directory("static", path)
 
-# endpoint to show highscores 
+
+# endpoint to show highscores
 @app.route("/highscores", methods=["GET"])
 def get_game():
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get("page", 1, type=int)
 
-    q = db.session.query(Game.id, Game.timestamp, Game.player, Game.level, func.max(Game.score).label('score'), Game.total_steps).group_by(Game.player).order_by(Game.score.desc(), Game.timestamp.desc())
-#    print(q.statement)
+    q = (
+        db.session.query(
+            Game.id,
+            Game.timestamp,
+            Game.player,
+            Game.puzzles,
+            Game.box_on_goal,
+            Game.total_pushes,
+            func.max(Game.score).label("score"),
+            Game.total_steps,
+        )
+        .group_by(Game.player)
+        .order_by(Game.score.desc(), Game.timestamp.desc())
+    )
+    #    print(q.statement)
 
     all_games = q.paginate(page, 20, False)
     result = games_schema.dump(all_games.items)
@@ -88,10 +139,15 @@ def get_game():
 # endpoint to show player games
 @app.route("/highscores/<player>", methods=["GET"])
 def game_detail(player):
-    game = db.session.query(Game).filter(and_(Game.player == player, Game.score > 20)).order_by(Game.score.desc()).limit(10)
+    game = (
+        db.session.query(Game)
+        .filter(and_(Game.player == player, Game.score > 20))
+        .order_by(Game.score.desc())
+        .limit(10)
+    )
     result = games_schema.dump(game)
     return jsonify(result)
 
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=80)
